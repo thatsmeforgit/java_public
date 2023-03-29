cf731ff08897ee8956184a340f747eccd87db006
Igor Alves <115327645+thatsmeforgit@users.noreply.github.com>
2023-03-29 16:17:17 -0300

    Create Ex1.java
    Exercício de Java para elevar um valor ao cubo, exercício comentado, feito em sala no dia 28/03/2023 na aula de Algoritmos e Estrutura de Dados.

---
+++ Ex1.java
@@ -0,0 +1,29 @@
+//importa a classe Scanner
+import java.util.Scanner;
+
+//declara a classe que o corpo do código
+class Ex1{
+    public static void main(String[] args){
+        //Declarando VARIÁVEIS em java: (tipo + nome_variavel, nome_variavel2...)
+        int valor, cubo;
+        Scanner entrada;
+
+        //ENTRADA
+        //objeto do tipo Scanner que passa como argumento (System.in) com objetivo de ler a entrada
+        entrada = new Scanner(System.in);
+
+        //PRINT PARA SOLICITAÇÃO DE ENTRADA
+        System.out.println("Valor: ");
+        //INVOCA UM MÉTODO DO TIPO INTEIRO PARA RETORNAR UM VALOR DO TIPO INTEIRO "nextTipoDado()" 
+        valor = entrada.nextInt();
+        //INFORMA O FIM DA ENTRADA PARA O SCANNER
+        entrada.close();
+
+        //PROCESS
+        cubo = valor * valor * valor;
+
+        //SAÍDA
+        //System.out.(printf) = Print com formatação ("%d recebe a variavel sucessivamente %d", variavel_1, variavel_2)
+        System.out.printf("%d ao cubo é %d\n", valor, cubo);
+    }
+}
